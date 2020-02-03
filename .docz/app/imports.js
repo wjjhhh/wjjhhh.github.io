2121f570af7e68229fc6327694b9606888e03bc9
user <user@userdeMacBook-Pro.local>
2020-02-03 23:58:06 +0800

    test

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -1,6 +1,4 @@
 export const imports = {
-  'index.md': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "index" */ 'index.md'),
   'src/index.mdx': () =>
     import(
       /* webpackPrefetch: true, webpackChunkName: "src-index" */ 'src/index.mdx'
